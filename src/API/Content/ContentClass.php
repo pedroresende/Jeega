3889e8e937894e9d74013cd40cd165aa9bd3e08f
Marcos Loureiro <mloureiro.dev@gmail.com>
2014-01-09 02:00:36 +0000

    First approach on ContentClass properties

--- src/API/Content/ContentClass.php
+++ src/API/Content/ContentClass.php
@@ -7,4 +7,29 @@ namespace Geega\API\Content;
 
 abstract class ContentClass {
 
+	// table id + database unique id
+	protected $id;
+	protected $remoteId;
+
+	// group
+	protected $groupId;
+
+	// creator + lastet editor
+	protected $createdBy;
+	protected $updatedBy;
+
+	// version
+	protected $version;
+
+	// timestamps
+	protected $createdAt;
+	protected $updatedAt;
+	protected $deletedAt;	// ???
+
+	// user changeable properties
+	public $names;
+	public $identifier;
+	public $container;
+	public $infoColector;
+
 }
