b21b5fd18ea20e6c8cc7e99830f5861648894d6f
Marcos Loureiro <mloureiro.dev@gmail.com>
2014-01-09 02:21:34 +0000

    Add some thoughs on how to handle information colector

--- src/API/Content/ContentClass.php
+++ src/API/Content/ContentClass.php
@@ -33,6 +33,13 @@ abstract class ContentClass {
 	public $names;
 	public $identifier;
 	public $container;
+	// $infoColector should go to field instead?
+	// how will the colected info handled?
+	// - possiblely it creates a class for the colected info and stores inside
+	//   the object, as it was an container, or maybe a setting to send it to
+	//   another object/container
+	// another approach would be to have the object collector and colected object
+	// view
 	public $infoColector;
 
 }
