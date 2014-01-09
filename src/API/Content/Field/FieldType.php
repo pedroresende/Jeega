5216da2f5826bc1280f754ee3ddb938def21f117
Marcos Loureiro <mloureiro.dev@gmail.com>
2014-01-09 01:24:34 +0000

    Add base classes for fields value and type

---
+++ src/API/Content/Field/FieldType.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File containing the FieldType class
+ */
+
+namespace Geega\API\Content\Field;
+
+interface FieldType {
+
+}
---
+++ src/API/Content/Field/FieldValue.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * File containing the FieldValue class
+ */
+
+namespace Geega\API\Content\Field;
+
+interface FieldValue {
+
+}
