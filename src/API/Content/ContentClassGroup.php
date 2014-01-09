9d16f0ea0d289a3296471029aac99c6d2b9b0b80
Marcos Loureiro <mloureiro.dev@gmail.com>
2014-01-09 02:05:59 +0000

    Add ContentClassGroup

---
+++ src/API/Content/ContentClassGroup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * File containing the ContentClass class
+ */
+
+namespace Geega\API\Content;
+
+abstract class ContentClassGroup {
+
+	// Id
+	protected $id;
+
+	// editable properties
+	public $names;
+}
