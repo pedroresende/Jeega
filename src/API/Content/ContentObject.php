071a618be8d72721c68cfc96c5c3aa3d1d014328
Marcos Loureiro <mloureiro.dev@gmail.com>
2014-01-09 02:17:03 +0000

    First approach on ContentObject properties

--- src/API/Content/ContentObject.php
+++ src/API/Content/ContentObject.php
@@ -7,4 +7,32 @@ namespace Geega\API\Content;
 
 abstract class ContentObject {
 
+	// table id + database unique id
+	protected $id;
+	protected $remoteId;
+
+	// class id
+	protected $classId;
+
+	// fields
+	protected $fields;
+
+	// creator + lastet editor
+	protected $createdBy;
+	protected $updatedBy;
+
+	// version and published
+	protected $version;
+	protected $status;
+	protected $published;
+
+	// timestamps
+	protected $createdAt;
+	protected $updatedAt;
+	protected $deletedAt;	// ???
+
+	// editable properties
+	public $names;
+	public $section;
+
 }
